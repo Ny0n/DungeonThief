bb74c7176310ab86798c58455334a52d486f39af
Ny0n <flav38320@gmail.com>
2021-10-05 15:19:13 +0200

    Commit projet initial

---
+++ GC_UE4CPP/Source/GC_UE4CPP/MainCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class GC_UE4CPP_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* BoomArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* Camera;
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
